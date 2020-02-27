0cc10fb1dce9b54b9009f8d1310e5abf94105177
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-02-27 13:40:12 +0000

    mvp + stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,89 @@
 import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
+import { Route, withRouter, Link } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
+import UpdateMovie from "./Movies/UpdateMovie";
+import axios from "axios";
+import AddMovie from "./Movies/AddMovie";
 
-const App = () => {
+const App = props => {
   const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
+  const [items, setItems] = useState([]);
 
-  const getMovieList = () => {
+  useEffect(() => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
-  };
+      .then(res => {
+        console.log("res.data", res.data);
+        setItems(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
-  useEffect(() => {
-    getMovieList();
-  }, []);
+  const updateItem = (id, item) => {
+    axios
+      .put(`http://localhost:5000/api/movies/${id}`, item)
+      .then(response => {
+        const updatedItem = response.data;
+        const newItems = items.map(item => {
+          if (item.id !== updatedItem.id) {
+            return item;
+          }
+          return updatedItem;
+        });
+        setItems(newItems);
+        props.history.push(`/`);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const deleteItem = id => {
+    axios
+      .delete(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        console.log("delete", response.data);
+        setItems(response.data);
+        props.history.push("/");
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <>
       <SavedList list={savedList} />
 
-      <Route exact path="/">
-        <MovieList movies={movieList} />
-      </Route>
-
-      <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
-      </Route>
+      <Route exact path="/" component={MovieList} />
+      <Route
+        exact
+        path="/add-movie"
+        render={props => <AddMovie {...props} items={items} />}
+      />
+      <Route
+        path="/update-movie/:id"
+        render={props => (
+          <UpdateMovie {...props} items={items} updateItem={updateItem} />
+        )}
+      />
+      <Route
+        path="/movies/:id"
+        render={props => {
+          return (
+            <Movie
+              {...props}
+              addToSavedList={addToSavedList}
+              items={items}
+              deleteItem={deleteItem}
+            />
+          );
+        }}
+      />
     </>
   );
 };
 
-export default App;
+export default withRouter(App);
